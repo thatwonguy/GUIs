c8838507f9b3bebfa543017d066089344e0765ed
MS <monsore1337@gmail.com>
2023-01-04 18:07:01 -0800

    commit

--- music_player/music_player.py
+++ music_player/music_player.py
@@ -11,7 +11,7 @@ window = tk.Tk()
 window.title("Music Player")
 
 #Load the music file
-pygame.mixer.music.load('music.ogg')
+pygame.mixer.music.load('music.mp3')
 
 # Create the play button
 def play():

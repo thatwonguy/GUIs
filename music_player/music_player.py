036ee850897085bb01a0a332affcce14b34eb6e4
thatwonguy <78534460+thatwonguy@users.noreply.github.com>
2023-01-04 16:25:42 -0800

    completed basic gui for music player

--- music_player/music_player.py
+++ music_player/music_player.py
@@ -10,3 +10,24 @@ window.title("Music Player")
 
 #Load the music file
 pygame.mixer.music.load('test_song.mp3')
+
+# Create the play button
+def play():
+  pygame.mixer.music.play()
+play_button = tk.Button(window, text="Play", command=play)
+play_button.pack()
+
+# Create the pause button
+def pause():
+  pygame.mixer.music.pause()
+pause_button = tk.Button(window, text="Pause", command=pause)
+pause_button.pack()
+
+# Create the stop button
+def stop():
+  pygame.mixer.music.stop()
+stop_button = tk.Button(window, text="Stop", command=stop)
+stop_button.pack()
+
+# Run the main event loop
+window.mainloop()

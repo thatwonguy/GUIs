055a891a156059286b97dfdd2f8adb3c48718375
MS <monsore1337@gmail.com>
2023-01-07 18:52:50 -0800

    resizing window and changing button locations

--- music_player/music_player.py
+++ music_player/music_player.py
@@ -4,10 +4,19 @@ import pygame
 #Initialize pygame
 pygame.mixer.init()
 
-pygame.display.set_mode((400, 300))
+#This will open up a pygame display window when program is run
+# pygame.display.set_mode((400, 300)) 
 
 #Create the main window
 window = tk.Tk()
+
+#Set the size of the window
+window.geometry('400x100')
+
+#All window to be resizable by user
+window.resizable(True,True)
+
+#Set title of GUI window
 window.title("Music Player")
 
 #Load the music file
@@ -17,31 +26,31 @@ pygame.mixer.music.load('music.mp3')
 def play():
   pygame.mixer.music.play()
 play_button = tk.Button(window, text="Play", command=play)
-play_button.pack()
+play_button.pack(side='left')
 
 # Create the fade out button
 def fadeout():
   pygame.mixer.music.fadeout(3000)
 fadeout_button = tk.Button(window, text="Fade Out", command=fadeout)
-fadeout_button.pack()
+fadeout_button.pack(side='left')
 
 # Create the pause button
 def pause():
   pygame.mixer.music.pause()
 pause_button = tk.Button(window, text="Pause", command=pause)
-pause_button.pack()
+pause_button.pack(side='left')
 
 # Create the unpause button
 def unpause():
   pygame.mixer.music.unpause()
 unpause_button = tk.Button(window, text="Unpause", command=unpause)
-unpause_button.pack()
+unpause_button.pack(side='left')
 
 # Create the stop button
 def stop():
   pygame.mixer.music.stop()
 stop_button = tk.Button(window, text="Stop", command=stop)
-stop_button.pack()
+stop_button.pack(side='left')
 
 # Run the main event loop
 window.mainloop() 
